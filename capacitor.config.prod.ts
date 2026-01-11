fe9755b4ec47dc800572021b30edf8e62a9ca170
Your Name <you@example.com>
2026-01-11 19:43:43 +0100

    Configure multi-environment build with Prod and Test apps

---
+++ capacitor.config.prod.ts
@@ -0,0 +1,23 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'de.svbolzum.vereinsmanager',
+    appName: 'Vereinsmanager',
+    webDir: 'www',
+    server: {
+        url: 'https://vereinsmanager-pro---prod.web.app/',
+        androidScheme: 'https',
+        allowNavigation: [
+            '*.web.app',
+            'vereinsmanager-pro*.web.app',
+            'vereinsmanager-cb*.web.app'
+        ]
+    },
+    plugins: {
+        PushNotifications: {
+            presentationOptions: ["badge", "sound", "alert"]
+        }
+    }
+};
+
+export default config;
---
+++ capacitor.config.test.ts
@@ -0,0 +1,23 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+    appId: 'de.svbolzum.vereinsmanager.test',
+    appName: 'Vereinsm. (Test)',
+    webDir: 'www',
+    server: {
+        url: 'https://vereinsmanager-cb966.web.app/',
+        androidScheme: 'https',
+        allowNavigation: [
+            '*.web.app',
+            'vereinsmanager-pro*.web.app',
+            'vereinsmanager-cb*.web.app'
+        ]
+    },
+    plugins: {
+        PushNotifications: {
+            presentationOptions: ["badge", "sound", "alert"]
+        }
+    }
+};
+
+export default config;
