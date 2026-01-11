d56d31e3113fd1373db5154bafbcfb2cfc811e12
Your Name <you@example.com>
2026-01-11 23:20:56 +0100

    Force Webview Cache Clear on Start

--- android/app/src/main/java/de/svbolzum/vereinsmanager/MainActivity.java
+++ android/app/src/main/java/de/svbolzum/vereinsmanager/MainActivity.java
@@ -1,5 +1,23 @@
 package de.svbolzum.vereinsmanager;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Force Clear Cache to ensure latest Web Content is loaded
+        if (this.bridge != null && this.bridge.getWebView() != null) {
+            this.bridge.getWebView().clearCache(true);
+            // Optional: Disable Cache completely if desired
+            // this.bridge.getWebView().getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        }
+    }
+}
